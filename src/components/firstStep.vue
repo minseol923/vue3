a34248a93447223db22f2e0feb0462ac7c70da3a
kimminseol <joo9_23@naver.com>
2023-11-27 21:01:15 +0900

    이메일, 비밀번호 localstorage 저장

--- src/components/firstStep.vue
+++ src/components/firstStep.vue
@@ -29,20 +29,44 @@ export default {
       confirmPassword: ''
     };
   },
+  created() {
+    // 페이지가 로드될 때 로컬 스토리지에서 email과 password를 불러옵니다.
+    this.email = localStorage.getItem('savedEmail') || '';
+    this.password = localStorage.getItem('savedPassword') || '';
+    this.confirmPassword = localStorage.getItem('savedConfirmPassword') || '';
+
+  },
   computed: {
     isFormValid() {
-      return this.email && this.password && this.confirmPassword && this.password === this.confirmPassword;
+      return (
+        this.email &&
+        this.password &&
+        this.confirmPassword &&
+        this.password === this.confirmPassword &&
+        this.isPassword(this.password)
+      );
     }
   },
   methods: {
     submitForm() {
       if (this.isFormValid) {
         console.log('폼이 제출되었습니다!');
+        // 로컬 스토리지에 email과 password 저장
+        localStorage.setItem('savedEmail', this.email);
+        localStorage.setItem('savedPassword', this.password);
+        localStorage.setItem('savedConfirmPassword', this.confirmPassword);
         this.$router.push('/secondStep'); 
+      } else if( this.password !== this.confirmPassword) {
+        alert('비밀번호 확인을 다시 입력해주세요.');
       } else {
-        console.log('유효하지 않은 입력입니다.');
+        alert('비밀번호에는 영문 대문자, 소문자, 숫자, 특수문자를 포함한 8자리 이상의 문자열이 필요합니다.');
       }
     },
+    //비밀번호 유효성 검사
+    isPassword(password) {
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+      return passwordRegex.test(password);
+    }
   }
 };
 </script>
